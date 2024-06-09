c76dbfb6393455bf6ac53516f962ab7344ba2295
Vaishali Sambhaji Jadhav <89831689+vaishalisjadhav@users.noreply.github.com>
2024-06-09 19:07:20 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+name="Vaishali"
+age=21
+clg="Keystone School of Engg. Pune"
+
+print("My name is",name)
+print("I am",age,"years old")
+print("I am pursing my degree at",clg)
+
+#print datatype
+print(type(name))
+print(type(age))
+
+
+#multiple assignment
+a,b,c=10,20,30
+print(a)
+print(b)
+print(c)
+
+#reassignment
+value=10
+print(value)
+value=20
+print(value)
+value=30
+value=40
+value=50
+print(value)
---
+++ variables.py
@@ -0,0 +1,18 @@
+name="Vaishali"
+print(name)
+
+college="KSOE, Pune"
+print(college)
+
+age=21
+print(age)
+
+branch="Computer"
+print(branch)
+
+#end variable
+print("Hello Everyone",end="\n")
+print("Good Morning")
+
+print("Hello Everyone",end=" ")
+print("Good Morning")
