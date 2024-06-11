8a861dc99b81db2e08eab487b87273fe62c2d61e
Vaishali Sambhaji Jadhav <89831689+vaishalisjadhav@users.noreply.github.com>
2024-06-11 13:17:54 +0530

    Comparison operators added

---
+++ Operators/Comparison-opr.py
@@ -0,0 +1,26 @@
+a=10
+b=20
+
+#Equal opeartor
+result= a==b
+print(result)
+
+#Less than opeartor
+result= a<b
+print(result)
+
+#Greater than opeartor
+result= a>b
+print(result)
+
+#Less than or equal
+result= a<=b
+print(result)
+
+#Greater than or equal
+result= a>=b
+print(result)
+
+#not equal
+result= a!=b
+print(result)
