d73cc590330c8f90a648854d08daa4bd2c3c1235
Vaishali Sambhaji Jadhav <89831689+vaishalisjadhav@users.noreply.github.com>
2024-06-11 13:17:19 +0530

    Arithmetic operators added

---
+++ Operators/arithmetic-opr.py
@@ -0,0 +1,19 @@
+a=2
+b=3
+
+Addition=a+b
+Subtraction=a-b
+Multiplication=a*b
+Division=a/b
+Modulus=a%b
+Exponentiation=a**b
+Floor=a//b
+
+
+print("Addition is", Addition)
+print("Subtraction is", Subtraction)
+print("Multiplication is", Multiplication)
+print("Division is", Division)
+print("Modulus is", Modulus)
+print("Exponentiation is", Exponentiation)
+print("Floor is", Floor)
